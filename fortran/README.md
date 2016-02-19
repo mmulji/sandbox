83bfdcfd883327ba800b7f68946068f579385fa8
Milan Mulji <milan.mulji@mmedia-cs.com>
2016-02-19 01:30:42 +0100

    chore: added fortran scripts and a number of empty projects

---
+++ fortran/README.md
@@ -0,0 +1,2 @@
+
+hello-world and module from http://www.tutorialspoint.com/fortran/fortran_modules.htm
---
+++ fortran/compile.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gfortran hello-world.f03 -o bin/hello-world
+gfortran module.f03 -o bin/module
+
